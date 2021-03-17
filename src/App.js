2881aa735de3c7233aff5f3a92be6a91135486c5
Utkarsh Goel <ugoel911@gmail.com>
2021-03-17 21:14:53 +0530

    private route added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Switch, Route } from "react-router-dom";
 import StudentNav from "./components/Navbar/student/studentnav";
 import TeacherNav from "./components/Navbar/teacher/teacherNav";
 import Login from "./components/Login/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={() => <Login />} />
-        <Route path="/student" component={() => <StudentNav />} />
-        <Route path="/teacher" component={() => <TeacherNav />} />
+        <PrivateRoute>
+          {" "}
+          <Route path="/student" component={() => <StudentNav />} />
+        </PrivateRoute>
+        <PrivateRoute>
+          <Route path="/teacher" component={() => <TeacherNav />} />
+        </PrivateRoute>
       </Switch>
       TITA
     </div>
