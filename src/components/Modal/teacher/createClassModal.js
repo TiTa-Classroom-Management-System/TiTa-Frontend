949d3c67424d5bd07bd8fb522b9050c3ad886d3f
solarconstant <ashishshubh001@gmail.com>
2021-03-17 18:15:45 +0530

    Remove minor warnings

--- src/components/Modal/teacher/createClassModal.js
+++ src/components/Modal/teacher/createClassModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { InputGroup, Input } from 'reactstrap';
 
