4047aedc8f37173b50e3c488a668fbeb800ce4b1
solarconstant <ashishshubh001@gmail.com>
2021-03-19 22:36:12 +0530

    Remove private routes

--- src/functions/classroom.js
+++ src/functions/classroom.js
@@ -3,4 +3,5 @@ import axios from "axios";
 export const createClassroom = async (classroomData) =>
 {
     await axios.post(`${process.env.REACT_APP_API}/api/classroom`, classroomData);
+    return;
 }
