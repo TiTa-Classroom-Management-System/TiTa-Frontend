51e34cc960a674d710de626b7381c633d23e0bad
Utkarsh Goel <ugoel911@gmail.com>
2021-03-27 18:48:48 +0530

    backend login connected

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,17 +3,41 @@ import { connect } from "react-redux";
 import GoogleLogin from "react-google-login";
 import { loginUser } from "../../redux/actions/userAction";
 import { withRouter } from "react-router-dom";
+import axios from "axios";
 import "./Login.css";
 
 const Login = (props) => {
   const dispatch = props.dispatch;
   const history = props.history;
-  const login = (res) => {
+  const login = async (res) => {
     var user = res.profileObj;
     if (user.email.includes("bt")) {
       user = { ...user, type: "student" };
     } else {
       user = { ...user, type: "teacher" };
+    }
+    if (user.type === "student") {
+      try {
+        await axios({
+          method: "post",
+          url: `${process.env.REACT_APP_API}/students/login`,
+          data: { name: user.name, email: user.email },
+          headers: { "Content-Type": "application/json" },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        await axios({
+          method: "post",
+          url: `${process.env.REACT_APP_API}/teachers/login`,
+          data: { name: user.name, email: user.email },
+          headers: { "Content-Type": "application/json" },
+        });
+      } catch (e) {
+        console.log(e);
+      }
     }
     dispatch(loginUser(user));
     if (user.type === "student") history.push("/student");
--- src/functions/classroom.js
+++ src/functions/classroom.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-export const createClassroom = async (classroomData) =>
-{
-    await axios.post(`${process.env.REACT_APP_API}/api/classroom`, classroomData);
-}
+export const createClassroom = async (classroomData) => {
+  await axios.post(`${process.env.REACT_APP_API}/api/classroom`, classroomData);
+};
