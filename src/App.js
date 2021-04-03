e7f576c34291663d4ff26ac05ad58054d1cf457c
solarconstant <ashishshubh001@gmail.com>
2021-04-03 13:22:59 +0530

    Add Timetable route and link

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Switch, Route } from "react-router-dom";
 import StudentNav from "./components/Navbar/student/studentnav";
 import TeacherNav from "./components/Navbar/teacher/teacherNav";
 import Login from "./components/Login/Login";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+// import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Timetable from "./pages/Timetable/Timetable";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/login" component={() => <Login />} />
-        <Route path="/student" component={() => <StudentNav />} />
-        <Route path="/teacher" component={() => <TeacherNav />} />
+        <Route path = "/login" component = {() => <Login />} />
+        <Route path = "/student" component = {() => <StudentNav />} />
+        <Route path = "/teacher" component = {() => <TeacherNav />} />
+        <Route path = "/timetable" component = {() => <Timetable />} />
       </Switch>
       TITA
     </div>
