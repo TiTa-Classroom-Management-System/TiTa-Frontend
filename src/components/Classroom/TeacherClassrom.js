79f7c2a2873f879e0269a5271b3d175b7e47c6eb
Utkarsh Goel <ugoel911@gmail.com>
2021-04-08 00:08:22 +0530

    teacher classroom display added

---
+++ src/components/Classroom/TeacherClassrom.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useParams, withRouter } from "react-router-dom";
+import { connect, useSelector } from "react-redux";
+import { updateTimetable } from "../../redux/actions/timetableAction";
+import axios from "axios";
+
+import Timetable from "../Timetable/Timetable";
+import TeacherNav from "../Navbar/teacher/teacherNav";
+
+import "../../pages/Timetable/Timetable.css";
+
+const TeacherTimetable = ({ dispatch, tt, params }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [timetable, setTimetable] = useState([]);
+  let { id } = useParams();
+
+  const loadTimetable = (user) => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_API}/teachers/timetable/${user.email}`,
+    })
+      .then((res) => {
+        dispatch(updateTimetable(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    loadTimetable(user);
+    setTimetable(() => tt.filter((t) => t.class_id == id));
+    console.log(timetable);
+  }, []);
+
+  return (
+    <div class="row">
+      <div class="col-12">
+        <TeacherNav />
+      </div>
+      <div class="col-11 Timetable__timetable-component">
+        {tt ? (
+          tt.length > 0 ? (
+            <Timetable tt={timetable} />
+          ) : (
+            <p>You have not created any timetable yet...</p>
+          )
+        ) : (
+          <p>Loading timetable...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const dispatchStateToProp = (state) => {
+  return { tt: state.timetable };
+};
+
+export default connect(dispatchStateToProp)(withRouter(TeacherTimetable));
