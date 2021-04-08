93f756352bb289b0a6a7e9fb1af45daf0355e32b
Puneet Bansal <puneetbansal15801@gmail.com>
2021-04-08 15:37:13 +0530

    resolved conflicts

--- src/components/Navbar/teacher/teacherNav.js
+++ src/components/Navbar/teacher/teacherNav.js
@@ -59,7 +59,6 @@ const TeacherNav = () => {
     setModal(!modal);
   };
 
-<<<<<<< HEAD
   return (
     <div>
       <Navbar light expand="lg">
@@ -80,7 +79,7 @@ const TeacherNav = () => {
             </Link>
           </NavItem>
           <NavItem>
-            <Link className="nav-link" to="#">
+            <Link className="nav-link" to="/teacherclassrooms">
               <i className="fa fa-users fa-2x" aria-hidden="true"></i>
               <span className="link-text"> Classrooms</span>
             </Link>
@@ -111,55 +110,4 @@ const TeacherNav = () => {
     </div>
   );
 };
-=======
-  const toggleModal = () =>
-  {
-      setModal(!modal);
-  }
-
-  return(
-      <div>
-          <Navbar light expand="lg">
-              <Nav navbar className="flex-column">
-                  <NavItem>
-                      <Link className="logo2" to="#">
-                          <img className="logo-image2" src={logo} alt="logo" />
-                          <span className="logo-text2"> <img src={logoText} alt="TITA" /></span>
-                      </Link>
-                  </NavItem>
-                  <NavItem>
-                      <Link className="nav-link" to="/teachtimetable">
-                          <i className="fa fa-calendar fa-2x" aria-hidden="true"></i>
-                          <span className="link-text"> Time table</span>
-                      </Link>
-                  </NavItem>
-                  <NavItem>
-                      <Link className="nav-link" to="/teacherclassrooms">
-                          <i className="fa fa-users fa-2x" aria-hidden="true"></i>
-                          <span className="link-text"> Classrooms</span>
-                      </Link>
-                  </NavItem>
-                  <NavItem onClick = {toggleModal}>
-                      <Link className="nav-link" to="#">
-                          <i className="fa fa-plus-circle fa-2x" aria-hidden="true"></i>
-                          <span className="link-text"> Create Classroom</span>
-                      </Link>
-                  </NavItem>
-              </Nav>
-          </Navbar>
-          {modal ? <ClassModal 
-                      toggle = {toggleModal} 
-                      modal = {modal} 
-                      className = "classModal"
-                      branches = {branchOptions}
-                      values = {values}
-                      setValues = {setValues} 
-                      handleChange = {handleChange}
-                      handleSubmit = {handleSubmit} /> : ""}
-          <Profile />
-      </div>
-  )
-}
-
->>>>>>> caefb1fb87a010fdabcdbadf0fb1b10449ab74ee
 export default TeacherNav;
