a225d9c5f106b1f7fc3e0174eade7a756153bb38
Taranjot Singh <taranjotsfeb2001@gmail.com>
2021-04-09 18:57:39 +0530

    Add Create Classroom Response

--- src/functions/classroom.js
+++ src/functions/classroom.js
@@ -3,17 +3,11 @@ import axios from "axios";
 export const createClassroom = async (classroomData) => 
 {
   const { subjectName, subjectCode, subGroups, email, branchName, branchYear } = classroomData;
-  await axios(
+  return await axios(
     {
       method: "POST",
       url: `${process.env.REACT_APP_API}/classroom/create`,
       data: { subjectName, subjectCode, subGroups, email, branchName, branchYear },
     }
-  ).then((res) =>
-  {
-    console.log("Classroom created.");
-  }).catch((err) =>
-  {
-    console.log(err);
-  })
+  )
 };
