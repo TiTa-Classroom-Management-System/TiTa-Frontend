c61e5b95c76be3602b6cc16d76cbbebedaaaa98f
Utkarsh Goel <ugoel911@gmail.com>
2021-04-20 17:14:04 +0530

    private router corrected

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,22 +3,16 @@ import { connect } from "react-redux";
 import { withRouter, Redirect, Route } from "react-router-dom";
 
 const PrivateRoute = ({ isLoggedIn, component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    component={(props) =>
-      isLoggedIn ? (
-        <div>
-          <Component {...props} />
-        </div>
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
-  />
+    <Route
+        {...rest}
+        component={(props) =>
+            isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />
+        }
+    />
 );
 
 const mapStateToProps = (state) => ({
-  isLoggedIn: !!state.user,
+    isLoggedIn: !!state.user,
 });
 
 export default connect(mapStateToProps)(withRouter(PrivateRoute));
