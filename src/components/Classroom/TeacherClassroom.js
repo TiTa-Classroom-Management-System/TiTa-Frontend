603cd1a94138c8b9416f89cf411cbed4ac85a308
Utkarsh Goel <ugoel911@gmail.com>
2021-04-21 20:21:51 +0530

    multiple groups in timetable added

--- src/components/Classroom/TeacherClassroom.js
+++ src/components/Classroom/TeacherClassroom.js
@@ -59,7 +59,7 @@ const TeacherClassroom = ({ dispatch, tt, classrooms, params }) => {
                 console.log(err);
             });
     };
-    
+
     const loadSubclassNum = () => {
         axios({
             method: "GET",
@@ -94,18 +94,20 @@ const TeacherClassroom = ({ dispatch, tt, classrooms, params }) => {
 
     const handleTimetableCreation = async () => {
         setModal(() => false);
-        await axios({
-            method: "POST",
-            url: `${process.env.REACT_APP_API}/timetable/create`,
-            data: {
-                class_id: id,
-                group_number: groups,
-                start_time: startTime,
-                end_time: endTime,
-                day: day,
-                type: type,
-            },
-        });
+        for (var i = 0; i < groups.length; i++) {
+            await axios({
+                method: "POST",
+                url: `${process.env.REACT_APP_API}/timetable/create`,
+                data: {
+                    class_id: id,
+                    group_number: groups[i],
+                    start_time: startTime,
+                    end_time: endTime,
+                    day: day,
+                    type: type,
+                },
+            });
+        }
         loadTimetable(user);
     };
 
