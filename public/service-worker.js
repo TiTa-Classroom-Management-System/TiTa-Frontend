f4a2d5af3bacfec5385c1344d57a36d5589efecb
Utkarsh Goel <ugoel911@gmail.com>
2021-04-28 12:36:03 +0530

    offline caching added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,42 @@
-importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js"
-);
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html", "logo.png", "../src"];
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === "image",
-    new workbox.strategies.NetworkFirst()
-);
+const self = this;
+
+// Install SW
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(() => {
+            return fetch(event.request).catch(() => caches.match("index.html"));
+        })
+    );
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
